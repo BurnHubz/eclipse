2b23e7d10370202541ff22fc33c18add93c82507
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-06 06:13:15 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -713,7 +713,6 @@ if not isfile("eclipse.wtf") then
                                 library.flags[args.flag] = state
                                 front.BackgroundColor3 = state and library.Colors.libColor or Color3.fromRGB(25, 25, 25)
                                 if args.risky then text.TextColor3 = state and Color3.fromRGB(255, 0, 0) or Color3.fromRGB(139, 0, 0) else text.TextColor3 = state and Color3.fromRGB(244, 244, 244) or Color3.fromRGB(144, 144, 144) end
-                                library:Tween(front, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundColor3 = state and library.libColor or Color3.fromRGB(21, 21, 21)})
                                 if args.callback then args.callback(state) end
                             end
 
