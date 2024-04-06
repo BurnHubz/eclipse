a5771446b6211461040b02f0fba0a0a667c156b1
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-06 06:19:51 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -701,7 +701,7 @@ if not isfile("eclipse.wtf") then
                                 front.Name = state and "accent" or "back"
                                 library.flags[args.flag] = state
                                 mid.BorderColor3 = Color3.fromRGB(20,20,20)
-                                front.BackgroundColor3 = state and library.Colors.libColor or Color3.fromRGB(25, 25, 25)
+                                --front.BackgroundColor3 = state and library.Colors.libColor or Color3.fromRGB(25, 25, 25)
                                 if args.risky then text.TextColor3 = state and Color3.fromRGB(255, 0, 0) or Color3.fromRGB(139, 0, 0) else text.TextColor3 = state and Color3.fromRGB(244, 244, 244) or Color3.fromRGB(144, 144, 144) end
                                 if args.callback then args.callback(state) end
                             else
@@ -712,7 +712,7 @@ if not isfile("eclipse.wtf") then
                                 state = newState
                                 library.flags[args.flag] = state
                                 --front.BackgroundColor3 = state and library.Colors.libColor or Color3.fromRGB(25, 25, 25)
-                                library:Tween(front, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundColor3 = state and library.libColor or Color3.fromRGB(21, 21, 21)})
+                                library:Tween(front, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundColor3 = state and library.libColor or Color3.fromRGB(25, 25, 25)})
                                 if args.risky then text.TextColor3 = state and Color3.fromRGB(255, 0, 0) or Color3.fromRGB(139, 0, 0) else text.TextColor3 = state and Color3.fromRGB(244, 244, 244) or Color3.fromRGB(144, 144, 144) end
                                 if args.callback then args.callback(state) end
                             end
@@ -722,8 +722,8 @@ if not isfile("eclipse.wtf") then
                             front.Name = state and "accent" or "back"
                             library.flags[args.flag] = state
                             mid.BorderColor3 = Color3.fromRGB(20,20,20)
-                            front.BackgroundColor3 = state and library.Colors.libColor or Color3.fromRGB(25, 25, 25)
-                            library:Tween(front, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundColor3 = state and library.libColor or Color3.fromRGB(21, 21, 21)})
+                            --front.BackgroundColor3 = state and library.Colors.libColor or Color3.fromRGB(25, 25, 25)
+                            library:Tween(front, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundColor3 = state and library.libColor or Color3.fromRGB(25, 25, 25)})
                             if args.risky then text.TextColor3 = state and Color3.fromRGB(255, 0, 0) or Color3.fromRGB(139, 0, 0) else text.TextColor3 = state and Color3.fromRGB(244, 244, 244) or Color3.fromRGB(144, 144, 144) end
                             if args.callback then args.callback(state) end
                             end)
