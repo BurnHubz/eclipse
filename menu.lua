2f2673b7dd094ca6c8bb0d2a469133843226dd3a
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-08 15:20:47 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -737,11 +737,11 @@ if not isfile("eclipse.wtf") then
                             end)
 
                             button.MouseEnter:Connect(function()
-                            library:Tween(mid, TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {BorderColor3 = library.Colors.libColor})
+                            library:Tween(mid, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {BorderColor3 = library.Colors.libColor})
                             end)
 
                             button.MouseLeave:Connect(function()
-                            library:Tween(mid, TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BorderColor3 = library.Colors.libColor})
+                            library:Tween(mid, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BorderColor3 = library.Colors.libColor})
                             end)
 
                             library.flags[args.flag] = false
