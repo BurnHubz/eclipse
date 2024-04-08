1bb1b1d932fb7b0e7d1630265b95440b7ed8077e
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-09 00:27:53 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -192,8 +192,8 @@ if not isfile("eclipse.wtf") then
     local WatermarkGui = CloneScreenGui
     local WatermarkFrame = Instance.new("Frame", WatermarkGui)
     local WaterMarkLabel = Instance.new("TextLabel", WatermarkFrame)
-    local WatermarkGradient = Instance.new("UIStroke", WatermarkFrame)
-    local WatermarkStroke = Instance.new("TextLabel", WatermarkFrame)
+    local WatermarkGradient = Instance.new("UIGradient", WatermarkFrame)
+    local WatermarkStroke = Instance.new("UIStroke", WatermarkFrame)
  
     WatermarkGui.Parent = CloneCore
     WatermarkGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
