16c24338348ce24d2be8fd663e546a6ad5616ee7
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-13 22:41:38 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,6 @@ writefile("ECLIPSE_KEY.txt", script_key)
 if creator_id == 1154360 and game.PlaceId == fallen_id then
     pcall(function()
         script_key = readfile("ECLIPSE_KEY.txt")
-        repeat wait() until game:GetService("Players").LocalPlayer.Character
         memorystats.cache("Gui")
         task.wait(.5);
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2e293addcf9a470164fe95eff5e92fc.lua"))()
