cc912f3fbe7d57732fd9075b3914d38ec438097c
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-13 21:20:09 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,15 @@
-repeat wait() until game:IsLoaded()
-local place_id = game.PlaceId;
+local FallenId = 10228136016
+writefile("ECLIPSE_KEY.txt", script_key)
 
-if (place_id == 10179538382) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d2dfa4cebd0410e773e815204a8edbe4.lua", true))()
-elseif (place_id == 10228136016) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua", true))()
-end
+if game.PlaceId == FallenId then
+        queue_on_teleport([[
+        pcall(function()
+            script_key = readfile("ECLIPSE_KEY.txt")
+            repeat task.wait() until getactors()[1] and getactors()[2] repeat wait() until game:GetService("Players").LocalPlayer.Character
+            task.wait(1);
+            memorystats.cache("Gui")
+            task.wait();
+            loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2e293addcf9a470164fe95eff5e92fc.lua"))()
+        end)
+    ]])
+end;
