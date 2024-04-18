d8322633d5aa279278fd84813a7316f42665c8aa
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-18 22:44:42 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,19 @@
 writefile("ECLIPSE_KEY.txt", script_key)
 
-if game.CreatorId == 1154360 and game.PlaceId ~= 10228136016 then
-    repeat wait() until game:GetService("Players").LocalPlayer.Character
-    pcall(function()
-        script_key = readfile("ECLIPSE_KEY.txt")
-        memorystats.cache("Gui")
-        task.wait(.5);
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2e293addcf9a470164fe95eff5e92fc.lua"))()
-    end)
-end;
+do
+    if game.PlaceId == 10228136016 then
+        local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/BurnHubz/eclipse/main/Loader.lua",true))()
+        Loader:Initialize()
+        --load
+        queue_on_teleport([==[
+            if game.CreatorId == 1154360 and game.PlaceId ~= 10228136016 then
+                pcall(function()
+                    script_key = readfile("ECLIPSE_KEY.txt")
+                    memorystats.cache("Gui")
+                    task.wait();
+                    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2e293addcf9a470164fe95eff5e92fc.lua"))()
+                end)
+            end;
+        ]==])
+    end
+end
