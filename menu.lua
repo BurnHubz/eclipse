3b09b808048d25d030531eca4ad05d3fb8ed1f20
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-04-22 21:37:52 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -965,8 +965,8 @@ local library = {
                                colorpicker.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
                                colorpicker.BorderColor3 = Color3.fromRGB(0, 0, 0)
                                colorpicker.BorderSizePixel = 3
-                               colorpicker.Position = args.second and UDim2.new(0.720000029, 4, 0.272000015, 0) or UDim2.new(0.860000014, 4, 0.272000015, 0)
-                               colorpicker.Size = UDim2.new(0, 15, 0, 10)
+                               colorpicker.Position = args.second and UDim2.new(0.720000029, 4, 0.272000015, 0) or UDim2.new(0.880000014, 4, 0.272000015, 0)
+                               colorpicker.Size = UDim2.new(0, 16, 0, 10)
 
                                mid.Name = "mid"
                                mid.Parent = colorpicker
