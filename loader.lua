52720fe99fb8b7353159dde59b09c2d162cbb72d
BurnHubz <114359748+BurnHubz@users.noreply.github.com>
2024-05-04 14:00:48 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 writefile("ECLIPSE_KEY.txt", script_key)
 
 do
-    if game.PlaceId == 10228136016 then
+    if game.PlaceId == 10228136016 then -- Fallen
         local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/BurnHubz/eclipse/main/Loader.lua",true))()
         Loader:Initialize()
         --load
@@ -15,5 +15,7 @@ do
                 end)
             end;
         ]==])
+    elseif game.PlaceId == 292439477 then -- Phantom Forces
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/06bf9037e2766eaba73fe866d4fad668.lua"))()
     end
 end
